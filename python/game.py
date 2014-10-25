addf594f5f7993aa2587869b4bc11b8ac888bc5c
Chris Jones <csj@andrew.cmu.edu>
2014-10-25 15:09:32 -0400

    hope it works

--- python/game.py
+++ python/game.py
@@ -36,9 +36,101 @@ class Point:
 
     def distance(self, point):
         return abs(point.x - self.x) + abs(point.y - self.y)
+    
+# rotates block 90deg counterclockwise
+def rotate_block(self, block, num_rotations):
+    return [offset.rotate(num_rotations) for offset in block]
+
+class GameState (object):
+    grid = []
+    blocks = [[], [], [], []]
+    player = -1
+    dimension = -1
+    turn = -1
+    bonus_squares = []
+
+    def __init__(self, grid, dim, blocks, t, p):
+        self.grid = grid
+        self.dimension = dim
+        self.blocks = blocks
+        self.turn = t
+        self.player = p
+
+    def inc_player(self):
+        self.player = (self.player + 1) % 4
+
+    def dec_player(self):
+        self.player = (self.player - 1) % 4
+
+    # Return a list of all valid moves from this board state
+    # Might optimize to get rid of useless moves (ones that don't look very useful)
+    def valid_moves(self):
+        moves = []
+        N = self.dimension
+        for index, block in enumerate(self.blocks[self.player]):
+            for i in range(0, N * N):
+                x = i / N
+                y = i % N
+                for rotations in range(0, 4):
+                    new_block = rotate_block(block, rotations)
+                    if self.can_place(new_block, Point(x, y)):
+                        moves.append(index, rotations, x, y)
+        return moves
+
+    # Checks if a block can be placed at the given point
+    def can_place(self, block, point):
+        onAbsCorner = False
+        onRelCorner = False
+        N = self.dimension - 1
+
+        corners = [Point(0, 0), Point(N, 0), Point(N, N), Point(0, N)]
+        corner = corners[self.player]
+
+        for offset in block:
+            p = point + offset
+            x = p.x
+            y = p.y
+            if (x > N or x < 0 or y > N or y < 0 or self.grid[x][y] != -1 or
+                (x > 0 and self.grid[x - 1][y] == self.player) or
+                (y > 0 and self.grid[x][y - 1] == self.player) or
+                (x < N and self.grid[x + 1][y] == self.player) or
+                (y < N and self.grid[x][y + 1] == self.player)
+            ): return False
+
+            onAbsCorner = onAbsCorner or (p == corner)
+            onRelCorner = onRelCorner or (
+                (x > 0 and y > 0 and self.grid[x - 1][y - 1] == self.player) or
+                (x > 0 and y < N and self.grid[x - 1][y + 1] == self.player) or
+                (x < N and y > 0 and self.grid[x + 1][y - 1] == self.player) or
+                (x < N and y < N and self.grid[x + 1][y + 1] == self.player)
+            )
+
+        if self.grid[corner.x][corner.y] < 0 and not onAbsCorner: return False
+        if not onAbsCorner and not onRelCorner: return False
+
+        return True
+
+
+    def apply_move(self, move, player):
+        locs = rotate_block(self.blocks[player][move[0]], move[1])
+        center = Point(move[2], move[3])
+        for offset in locs:
+            p = center + offset
+            self.state[p.x][p.y] = player
+        self.turn += 1
+        return self.blocks[player].pop(move[0])
+    
+    def undo_move(self, move, player, block):
+        self.apply_move(state, move, -1)
+        self.blocks[player].insert(move[0], block)
+        self.turn -= 1
+
+    # The move that got you to current state
+    def estimate(move):
+        return 0
 
 class Game (object):
-    all_blocks = []
+   """ all_blocks = []
     blocks = []
     grid = []
     bonus_squares = []
@@ -46,6 +138,11 @@ class Game (object):
     dimension = -1 # Board is assumed to be square
     turn = -1
     max_depth = 1
+"""
+
+    state = None
+    my_number = -1
+    turn = -1
 
     def __init__(self, args):
         self.interpret_data(args)
@@ -61,91 +158,49 @@ class Game (object):
         best_move, best_score = self.search(self.grid, moves, self.max_depth, self.my_number)
         return best_move
 
-    def apply_move(self, board, move):
-        pass
-
-    def search(self, board, moves, depth, player):
+    def search(self, state, moves, depth):
         best_seen = 0
         best_move = (0,0,0,0)
         for move in moves:
-            next_board = self.apply_move(board, move)
-            board_val = self.evaluate3(next_board, move, depth, player)
+            block_played = state.apply_move(move, state.player)
+            state.inc_player()
+            board_val = self.evaluate3(state, move, depth)
             if board_val > best_seen:
                 best_seen = board_val
                 best_move = move
+            state.dec_player()
+            state.undo_move(move, state.player, block_played)
+            state.dec_player()
 
         return best_seen, best_move
 
-    def evaluate3(self, board, move, depth, player):
+    def evaluate3(self, state, move, depth):
         if not depth:
-            return self.estimate(board, move, player)
-        currently_playing = player
-        current_board = board
+            return state.estimate(move)
+        blocks_played = []
+        moves_played = []
+        enemy_scores = []
         for i in xrange(3):
-            currently_playing = self.next_player(currently_playing)
-            his_score, his_move = self.search(current_board, self.get_valid_moves(current_board, currently_playing), depth - 1, currently_playing)
+            currently_playing = state.player
+            his_score, his_move = self.search(state, state.valid_moves(), depth - 1)
+            blocks_played.append(state.apply_move(his_move, currently_playing))
+            moves_played.append(his_move)
+            enemy_scores.append(his_score)
+            state.inc_player()
 
-            current_board = self.apply_move(current_board, his_move)
-        my_score, my_move = self.search(current_board, self.get_valid_moves(current_board, player), depth - 1, player)
+        # Could do some magic here about the his_score list
+
+        my_score, my_move = self.search(state, state.valid_moves(), depth - 1)
+        for i in xrange(3):
+            state.dec_player()
+            state.undo_move(moves_played[2 - i], state.player, blocks_played[2 - i])
         return my_score
-
-
-    def estimate(self, board, move, player):
-        return 0
-
-    def get_valid_moves(self):
-        moves = []
-        N = self.dimension
-        for index, block in enumerate(self.blocks):
-            for i in range(0, N * N):
-                x = i / N
-                y = i % N
-                for rotations in range(0, 4):
-                    new_block = self.rotate_block(block, rotations)
-                    if self.can_place(new_block, Point(x, y)):
-                        moves.append(index, rotations, x, y)
-        return moves
-
-
-    # Checks if a block can be placed at the given point
-    def can_place(self, block, point):
-        onAbsCorner = False
-        onRelCorner = False
-        N = self.dimension - 1
-
-        corners = [Point(0, 0), Point(N, 0), Point(N, N), Point(0, N)]
-        corner = corners[self.my_number]
-
-        for offset in block:
-            p = point + offset
-            x = p.x
-            y = p.y
-            if (x > N or x < 0 or y > N or y < 0 or self.grid[x][y] != -1 or
-                (x > 0 and self.grid[x - 1][y] == self.my_number) or
-                (y > 0 and self.grid[x][y - 1] == self.my_number) or
-                (x < N and self.grid[x + 1][y] == self.my_number) or
-                (y < N and self.grid[x][y + 1] == self.my_number)
-            ): return False
-
-            onAbsCorner = onAbsCorner or (p == corner)
-            onRelCorner = onRelCorner or (
-                (x > 0 and y > 0 and self.grid[x - 1][y - 1] == self.my_number) or
-                (x > 0 and y < N and self.grid[x - 1][y + 1] == self.my_number) or
-                (x < N and y > 0 and self.grid[x + 1][y - 1] == self.my_number) or
-                (x < N and y < N and self.grid[x + 1][y + 1] == self.my_number)
-            )
-
-        if self.grid[corner.x][corner.y] < 0 and not onAbsCorner: return False
-        if not onAbsCorner and not onRelCorner: return False
-
-        return True
-
-    # rotates block 90deg counterclockwise
-    def rotate_block(self, block, num_rotations):
-        return [offset.rotate(num_rotations) for offset in block]
 
     # updates local variables with state from the server
     def interpret_data(self, args):
+        if (('move' not in args) or (args['move'] != 1)):
+            return
+            
         if 'error' in args:
             debug('Error: ' + args['error'])
             return
@@ -154,18 +209,19 @@ class Game (object):
             self.my_number = args['number']
 
         if 'board' in args:
-            self.dimension = args['board']['dimension']
-            self.turn = args['turn']
-            self.grid = args['board']['grid']
-            self.allblocks = args['blocks']
-            self.blocks = args['blocks'][self.my_number]
-            self.bonus_squares = args['board']['bonus_squares']
+            dimension = args['board']['dimension']
+            turn = args['turn']
+            grid = args['board']['grid']
+            allblocks = args['blocks']
+            bonus_squares = args['board']['bonus_squares']
 
-            for index, block in enumerate(self.blocks):
-                self.blocks[index] = [Point(offset) for offset in block]
+        for blocks in allblocks:
+            for index, block in enumerate(blocks):
+                blocks[index] = [Point(offset) for offset in block]
 
-        if (('move' in args) and (args['move'] == 1)):
-            send_command(" ".join(str(x) for x in self.find_move()))
+        self.state = GameState(grid, dimension, allblocks, turn, bonus_squares, self.my_number)
+
+        send_command(" ".join(str(x) for x in self.find_move()))
 
     def is_my_turn(self):
         return self.turn == self.my_number
